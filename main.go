0ea10b2333c5a5cf2b6636f25706eb3193e1a867
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-06-03 18:39:20 +0800

    Building CRUD endpoints without Database Connection

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/lpernett/godotenv"
 )
 
 type Todo struct {
@@ -17,11 +20,19 @@ func main() {
 
 	todos := []Todo{}
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	// GET ALL TODOS
 	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(todos)
 	})
 
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
+	// CREATE A TODO
+	app.Post("/api/todos/", func(c *fiber.Ctx) error {
 		todo := &Todo{}
 
 		if err := c.BodyParser(todo); err != nil {
@@ -30,13 +41,57 @@ func main() {
 
 		if todo.Body == "" {
 			return c.Status(400).JSON(fiber.Map{"msg": "Body is required"})
-
 		}
+
 		todo.ID = len(todos) + 1
 		todos = append(todos, *todo)
 
 		return c.Status(201).JSON(todo)
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	// UPDATE A TODO
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+
+		todo := new(Todo)
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid input"})
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				if todo.Body != "" {
+					todos[i].Body = todo.Body
+				}
+				todos[i].Completed = todo.Completed
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	// DELETE A TODO
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"msg": "Todo deleted", "id": id, "success": "true"})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
 }
